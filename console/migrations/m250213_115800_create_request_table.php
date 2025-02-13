c6a68fb79efc09da33fe09918545fb6b4bbb7e5c
Gennady Lukinov <gennadylukinov@yandex.ru>
2025-02-13 21:19:45 +0300

    Bugfix Added UTF-8 support to create_request_table migration.

--- console/migrations/m250213_115800_create_request_table.php
+++ console/migrations/m250213_115800_create_request_table.php
@@ -12,6 +12,12 @@ class m250213_115800_create_request_table extends Migration
      */
     public function safeUp()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // https://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
         $this->createTable('{{%request}}', [
             'id' => $this->primaryKey(),
             'created_at' => $this->integer()->notNull(),
@@ -21,7 +27,7 @@ class m250213_115800_create_request_table extends Migration
             'status' => $this->tinyInteger()->notNull()->defaultValue(0),
             'message' => $this->text()->notNull(),
             'comment' => $this->text(),
-        ]);
+        ], $tableOptions);
     }
 
     /**
