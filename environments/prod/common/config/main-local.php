9414b9929bc7dbfcb6ea6c738fdc47aaa236353f
Gennady Lukinov <gennadylukinov@yandex.ru>
2025-02-13 14:33:25 +0300

    Initial commit Created a new project using Yii Framework 2.

---
+++ environments/prod/common/config/main-local.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'components' => [
+        'db' => [
+            'class' => \yii\db\Connection::class,
+            'dsn' => 'mysql:host=localhost;dbname=demo',
+            'username' => 'demo',
+            'password' => 'demo',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => \yii\symfonymailer\Mailer::class,
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
